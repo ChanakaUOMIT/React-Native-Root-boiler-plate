4412af207632c5f5cf170ddd3402955c3077d727
Chanaka Sampath Wickramanayaka <chanakauomfit@gmail.com>
2018-12-06 11:39:21 +0530

    login navigate

--- src/authScreen/login/Login.js
+++ src/authScreen/login/Login.js
@@ -1,19 +1,29 @@
 import React, { Component } from 'react';
 import {View,
-    Text,
     StyleSheet,
     TouchableOpacity,
 } from 'react-native';
+import { Container, Header, Content, Button, Text } from 'native-base';
  
 class Login extends Component{
 constructor(props){
     super(props);
 
 } 
+
+    loginHandler=()=>{
+        this.props.navigation.navigate('DrewerNav')
+    }
      render(){
         return(
             <View style={styles.container}> 
                 <Text> Login </Text>
+                <View style={{alignItems:'center'}}>
+                    <Button onPress={this.loginHandler}>
+                        <Text>Click Me!</Text>
+                    </Button>
+                </View>
+                
              </View>
          )
      }
